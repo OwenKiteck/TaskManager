ac05a38ce84d4633a082aa64084efadefc02146f
Student <owen.kiteck@stu.barren.kyschools.us>
2022-03-02 09:56:13 -0600

    Saved files

---
+++ tasks.js
@@ -0,0 +1,9 @@
+
+
+prompt(`TASK MANAGER
+
+What would you like to do (Please enter one of the options below):
+"TASKS" - Display all tasks
+"NEW" - Add a new task
+"REMOVE" - Remove a task
+"CLOSE" Close the task manager`)
